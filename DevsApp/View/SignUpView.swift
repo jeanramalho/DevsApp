1cbc4bd42ccfa1eb553904c7a2fd7223af336713
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-10-17 15:58:13 -0300

    [feat] - Cria elementos do layout da view de cadastro

--- DevsApp/View/SignUpView.swift
+++ DevsApp/View/SignUpView.swift
@@ -9,6 +9,45 @@ import UIKit
 
 class SignUpView: UIView {
     
+    private let nameTextField: PaddedTextField = {
+        let textField = PaddedTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Digite seu nome..."
+        return textField
+    }()
+    
+    private let emailTextField: PaddedTextField = {
+        let textField = PaddedTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Digite seu email..."
+        return textField
+    }()
+    
+    private let passwordTextField: PaddedTextField = {
+        let textField = PaddedTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Digite sua senha..."
+        return textField
+    }()
+    
+    private let confirmPasswordTextField: PaddedTextField = {
+        let textField = PaddedTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Confirme sua senha..."
+        return textField
+    }()
+    
+    private let singUpButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Cadastrar", for: .normal)
+        button.backgroundColor = Colors.bgColor
+        button.layer.cornerRadius = 6
+        button.layer.borderWidth = 1
+        button.layer.borderColor = Colors.bluePrimary.cgColor
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
