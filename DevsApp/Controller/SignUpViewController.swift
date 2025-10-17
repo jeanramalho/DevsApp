552ad723aa084a3bbd2d3e377661d0f8206adc94
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-10-17 15:47:57 -0300

    [feat] - Cria view controller da tela de cadastro

---
+++ DevsApp/Controller/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 17/10/25.
+//
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    private let contentView: SignUpView = SignUpView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
