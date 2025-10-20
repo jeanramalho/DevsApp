6a6f2c86c904245857109b98e716f6d2dd23ca15
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-10-20 15:35:15 -0300

    [feat] - Cria Views e estrutura primária das viewControllers da MainTabBarController

---
+++ DevsApp/View/MainTabBarViews/ChatsView.swift
@@ -0,0 +1,34 @@
+//
+//  ChatsView.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 20/10/25.
+//
+import UIKit
+import Foundation
+
+class ChatsView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        
+    }
+    
+    private func setConstraints(){
+        
+    }
+}
---
+++ DevsApp/View/MainTabBarViews/SettingsView.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsView.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 20/10/25.
+//
+import Foundation
+import UIKit
+
+class SettingsView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        
+    }
+    
+    private func setConstraints(){
+        
+    }
+}
