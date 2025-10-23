67640048f9ff0ab19f5b275fac76b2ccfc33f98c
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-10-23 00:07:56 -0300

    [feat] - Inicia desenvolvimento da celula de chat

---
+++ DevsApp/View/TableViewCells/ChatTableViewCell/ChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTableViewCell.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 23/10/25.
+//
+import Foundation
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "ChatTableViewCell"
+    
+    private let 
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
