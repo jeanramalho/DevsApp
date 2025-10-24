1ea5e0bba47a14c5a25de21cbd06dfb5d4a28708
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-10-24 01:28:26 -0300

    [feat] - Implementa estrutura principal da view de contatos

--- DevsApp/View/MainTabBarViews/ContactsView.swift
+++ DevsApp/View/MainTabBarViews/ContactsView.swift
@@ -11,10 +11,26 @@ class ContactsView: UIView {
     
     public let headerHeightContacts: CGFloat = 110
     
+    lazy var navContactsTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+    
     private let headerView: HeaderView = {
         let headerView = HeaderView(title: "Contatos")
         headerView.translatesAutoresizingMaskIntoConstraints = false
         return headerView
+    }()
+    
+    lazy var contactsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = Colors.bgColor
+        return tableView
     }()
     
     override init(frame: CGRect) {
@@ -30,20 +46,34 @@ class ContactsView: UIView {
         
         backgroundColor = Colors.bgColor
         
+        setupTableHeader()
         setHierarchy()
         setConstraints()
     }
     
+    private func setupTableHeader(){
+        
+        // Define ajustes da headerView
+        headerView.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: headerHeightContacts)
+        headerView.layoutIfNeeded()
+        
+        // Define tableHeaderView
+        contactsTableView.tableHeaderView = headerView
+    }
+    
     private func setHierarchy(){
         
-        addSubview(headerView)
+        addSubview(contactsTableView)
     }
     
     private func setConstraints(){
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+            
+            // Define constraints da tableView
+            contactsTableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            contactsTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            contactsTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            contactsTableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
 }
