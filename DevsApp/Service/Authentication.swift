f452949b3a6543d1f72987f7b3774e52c11aa456
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-11-18 10:57:58 -0300

    [feat] - Inicia desenvolvimento da lógica do Service

---
+++ DevsApp/Service/Authentication.swift
@@ -0,0 +1,14 @@
+//
+//  Authentication.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 18/11/25.
+//
+import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class Authentication {
+    
+}
