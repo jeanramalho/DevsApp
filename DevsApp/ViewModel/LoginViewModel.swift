7d50b289213d1f6ece1f2307c17cfb97a23200b4
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-11-25 14:51:54 -0300

    [feat] - Cria arquivo de viewmodel para a viewcontroller de login

---
+++ DevsApp/ViewModel/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewModel.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 25/11/25.
+//
+import Foundation
+
+final class LoginViewModel: NSObject {
+    
+    private let auth: Authentication = Authentication()
+    
+    override init(){
+        super.init()
+    }
+    
+    
+}
