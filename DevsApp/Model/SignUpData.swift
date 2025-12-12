762c1b70a2deb330a952499334e370743f96c73f
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-12-12 12:27:02 -0300

    [feat] - Cria Model dos dados de singUp

---
+++ DevsApp/Model/SignUpData.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpData.swift
+//  DevsApp
+//
+//  Created by Jean Ramalho on 12/12/25.
+//
+import Foundation
+
+struct SignUpData {
+    let name: String
+    let email: String
+    let password: String
+    let confirmPassword: String
+}
