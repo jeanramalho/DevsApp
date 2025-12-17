1c2da926e7661cc1b73850bb2c41afb6b352dfa1
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-12-17 13:31:37 -0300

    [feat] - Cria validações dos campos para criação dos usuários

--- DevsApp/ViewModel/SingUpViewModel.swift
+++ DevsApp/ViewModel/SingUpViewModel.swift
@@ -23,5 +23,34 @@ final class SignUpViewModel: SignUpViewModelType {
     }
     
     // MARK: - SignUp
-    
+    func signUp(name: String, email: String, password: String, confirmPassword: String){
+        
+        // Validações do parametros
+        let trimmedName = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        let trimmedEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        let trimmedPassword = password.trimmingCharacters(in: .whitespacesAndNewlines)
+        let trimmedConfirm = confirmPassword.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmedName.isEmpty else {
+            signUpState.send(.failure(.custom(message: "Informe seu nome!")))
+            return
+        }
+        
+        guard !trimmedEmail.isEmpty else {
+            signUpState.send(.failure(.custom(message: "Infrome seu email!")))
+            return
+        }
+        
+        guard trimmedPassword.count >= 6 else {
+            signUpState.send(.failure(.custom(message: "A senha deve ter pelo menos 6 caracteres!")))
+            return
+        }
+        
+        guard trimmedPassword == trimmedConfirm else {
+            signUpState.send(.failure(.custom(message: "As senhas não conferem!")))
+            return
+        }
+        
+        // Se passar nas validações -> Solicita criação
+    }
 }
